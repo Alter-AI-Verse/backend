cb2da91dbf07c9b52337cf6b9815529a9b6ea62c
dev-debasis <debasiskhamari7@gmail.com>
2025-02-22 15:01:09 +0530

    feat(auth): Implement authentication controllers and other configurations

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import express, { urlencoded } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
+import uploadRoutes from "./routes/upload.routes.js"
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+app.use(urlencoded(
+{ extended: true, 
+    limit: "20kb",
+}));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.send('Persona-Digital version of You !!');
+})
+app.use("/api/v1/auth", authRoutes)
+app.use("/api/v1/upload", uploadRoutes)
+
+export { app };
